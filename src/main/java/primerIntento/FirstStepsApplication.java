f389d16a3ec66ac9c9bd1343e2f3189f0a4f29c9
cabrera-man <casiodc@gmail.com>
2011-05-05 23:55:16 -0300

    Primer commit mavenizado con: Restlet.

---
+++ src/main/java/primerIntento/FirstStepsApplication.java
@@ -0,0 +1,25 @@
+package primerIntento;
+
+import org.restlet.Application;
+import org.restlet.Restlet;
+import org.restlet.routing.Router;
+
+public class FirstStepsApplication extends Application {
+
+    /**
+     * Creates a root Restlet that will receive all incoming calls.
+     */
+    @Override
+    public synchronized Restlet createInboundRoot() {
+        // Create a router Restlet that routes each call to a new instance of HelloWorldResource.
+        Router router = new Router(getContext());
+
+        // Defines only one route
+        router.attach("/hello", HelloWorldResource.class);
+        router.attach("/bye", HelloWorldResource.class);
+
+
+        return router;
+    }
+
+}   
---
+++ src/main/java/primerIntento/HelloWorldResource.java
@@ -0,0 +1,16 @@
+package primerIntento;
+
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+/**
+ * Resource which has only one representation.
+ */
+public class HelloWorldResource extends ServerResource {
+
+    @Get
+    public String represent() {
+        return "hello, world";
+    }
+
+}
