b333f4114aff02d65ae0f43d80e6ae47cf888a8f
santiagovillarreal <santiago@villarreal.net.ar>
2011-05-10 01:29:49 -0700

    Parseo del json con json-lib. Cambié el producto del ítem, ahora es un redoblante (tambor) que se vende en ML. El test está un poco más enriquecido. Mirar el nuevo assertTrue.

--- src/test/java/ar/edu/utn/tacs2011c1/test/cliente/TestCliente.java
+++ src/test/java/ar/edu/utn/tacs2011c1/test/cliente/TestCliente.java
@@ -1,26 +1,27 @@
 package ar.edu.utn.tacs2011c1.test.cliente;
 
 import static org.junit.Assert.*;
-
 import java.io.IOException;
-
 import org.junit.Test;
+import org.restlet.data.MediaType;
 import org.restlet.resource.ClientResource;
 import org.restlet.resource.ResourceException;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
 
 public class TestCliente {
-		
-	  
-	
+
 	@Test
-	public void testGetApiML() throws ResourceException, IOException{
-	ClientResource resource = new ClientResource("https://api.mercadolibre.com/items/MLA110394682");  
-	 
-	resource.get().write(System.out);
+	public void testGetApiML() throws ResourceException, IOException {
+		ClientResource resource = new ClientResource(
+				"https://api.mercadolibre.com/items/MLA114618178");
+		String jsonString = resource.get(MediaType.APPLICATION_JSON).getText();
 
-	
-	assertTrue(true);
+		JSONObject itemJson = (JSONObject) JSONSerializer.toJSON(jsonString);
+
+		String title = itemJson.getString("title");
+
+		assertTrue(title.equals(new String("Tambor Mapex Biack Panter")));
+
 	}
-
-	
 }
