bf2f1d07eb31fc0ebe9735be306b49433887bdb2
cabrera-man <casiodc@gmail.com>
2011-06-07 01:20:48 -0300

    Modificaciones necesarias para la 3ra entrega, falta hacer TDD el resto funciona perfectamente

--- src/main/webapp/js/ml.js
+++ src/main/webapp/js/ml.js
@@ -20,7 +20,9 @@ function cargarCategoria(category_id) {
 			$("#main_categories li#" + category_id).attr("cargado", "yes");
 			$("#main_categories li#" + category_id).append(nuevo_ul);
 			nuevo_ul.slideDown();
-		}
+		},
+		beforeSend: logoCargandoShowHide,
+		complete: logoCargandoShowHide
 	});
 }; // fin cargarCategoria()
 
@@ -36,7 +38,9 @@ function cargarCategoriasPrincipal() {
 				lista_item = creadorItem(categoria);
 				$("#main_categories").append(lista_item);
 			});
-		}
+		},
+		beforeSend: logoCargandoShowHide,
+		complete: logoCargandoShowHide
 	});
 }; // fin cargarCategoriasPrincipal()
 
@@ -54,13 +58,76 @@ function creadorItem(categoria) {
 	});
 
 	lista_item.append($("<span>" + categoria.name + "</span>"));
+	
+	linkMostrar = $("<span>Ver</span>");
+	linkMostrar.addClass("linkMostrar");
+	linkMostrar.click(function(){
+		buscarArticuloXString("category=" + category_id);
+	});
+	
+	lista_item.append(linkMostrar);
 
 	return lista_item;
 }// fin creadorItem()
+
+/****Animación de espera*/
+function logoCargandoShowHide(){
+	$("#logoCargando").fadeToggle();
+}
+
+/****accion cuando se realiza una busqueda
+*/
+
+function buscarArticuloXString(strBusqueda, ulDondeCargar){
+	var URL = "https://api.mercadolibre.com/sites/MLA/search?" + strBusqueda;
+	var ulFinal = ulDondeCargar;
+	$.ajax({
+		type : "GET",
+		url : URL,
+		timeout : 10000,
+		dataType : "jsonp",
+		success : function(data, status) {
+			ulResultados = $("#listaBusqueda");
+			ulResultados.fadeToggle(function(){
+				$("#listaBusqueda").empty();
+
+				$.each(data[2].results, function(i, resultado) {
+					lista_item = crearElementoResultado(resultado);
+					$("#listaBusqueda").append(lista_item);
+				});
+			});
+			ulResultados.fadeToggle();
+		},
+		beforeSend: logoCargandoShowHide,
+		complete: logoCargandoShowHide
+
+
+	});
+}
+
+function crearElementoResultado(resultado){
+	nuevoItem = $("<li></li>");
+	
+	imgMiniatura = $("<img />");
+	imgMiniatura.attr("src", resultado.thumbnail);
+	
+	nuevoItem.append(imgMiniatura);
+	nuevoItem.append(resultado.title);
+	
+	return nuevoItem;
+}
+
+
+
 
 /**
  * ***** Inicializo el .html y le digo que cargue las primeras categorias
  */
 $(document).ready(function() {
 	cargarCategoriasPrincipal();
+	
+	$("#formBuscar").submit( function(){
+			buscarArticuloXString("q=" + $("#txtBuscar").val());
+			return false;
+	});
 });
